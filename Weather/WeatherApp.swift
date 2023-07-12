decbb0f012a305dc67d83d0d7f1658797b863421
DenShizik <denisdikarvit@gmail.com>
2023-07-12 20:12:21 +0200

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Денис Сторожев on 12.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
